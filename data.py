ad68328f9b7ed93f516b602219062464bd8807be
Tania-Bell <taniabell1@hotmail.com>
2023-02-02 18:06:36 -0600

    Creation of functions
    Changed the common tickers and data import into functions

--- data.py
+++ data.py
@@ -5,39 +5,10 @@ Created on Mon Jan 30 19:50:15 2023
 @author: Tania
 """
 
-import pandas as pd
 
-# nt_1 = pd.read_csv("NAFTRAC_20210129.csv",skiprows=2).iloc[:-1 , :]
-# nt_2 = pd.read_csv("NAFTRAC_20210226.csv",skiprows=2).iloc[:-1 , :]
-# nt_3 = pd.read_csv("NAFTRAC_20210331.csv",skiprows=2).iloc[:-1 , :]
-# nt_4 = pd.read_csv("NAFTRAC_20210430.csv",skiprows=2).iloc[:-1 , :]
-# nt_5 = pd.read_csv("NAFTRAC_20210531.csv",skiprows=2).iloc[:-1 , :]
-# nt_6 = pd.read_csv("NAFTRAC_20210630.csv",skiprows=2).iloc[:-1 , :]
-# nt_7 = pd.read_csv("NAFTRAC_20210730.csv",skiprows=2).iloc[:-1 , :]
-# nt_8 = pd.read_csv("NAFTRAC_20210831.csv",skiprows=2).iloc[:-1 , :]
-# nt_9 = pd.read_csv("NAFTRAC_20210930.csv",skiprows=2).iloc[:-1 , :]
-# nt_10 = pd.read_csv("NAFTRAC_20211026.csv",skiprows=2).iloc[:-1 , :]
-# nt_11 = pd.read_csv("NAFTRAC_20211130.csv",skiprows=2).iloc[:-1 , :]
-# nt_12 = pd.read_csv("NAFTRAC_20211231.csv",skiprows=2).iloc[:-1 , :]
-# nt_13 = pd.read_csv("NAFTRAC_20220126.csv",skiprows=2).iloc[:-1 , :]
-# nt_14 = pd.read_csv("NAFTRAC_20220228.csv",skiprows=2).iloc[:-1 , :]
-# nt_15 = pd.read_csv("NAFTRAC_20220331.csv",skiprows=2).iloc[:-1 , :]
-# nt_16 = pd.read_csv("NAFTRAC_20220429.csv",skiprows=2).iloc[:-1 , :]
-# nt_17 = pd.read_csv("NAFTRAC_20220531.csv",skiprows=2).iloc[:-1 , :]
-# nt_18 = pd.read_csv("NAFTRAC_20220630.csv",skiprows=2).iloc[:-1 , :]
-# nt_19 = pd.read_csv("NAFTRAC_20220729.csv",skiprows=2).iloc[:-1 , :]
-# nt_20 = pd.read_csv("NAFTRAC_20220831.csv",skiprows=2).iloc[:-1 , :]
-# nt_21 = pd.read_csv("NAFTRAC_20220930.csv",skiprows=2).iloc[:-1 , :]
-# nt_22 = pd.read_csv("NAFTRAC_20221031.csv",skiprows=2).iloc[:-1 , :]
-# nt_23 = pd.read_csv("NAFTRAC_20221130.csv",skiprows=2).iloc[:-1 , :]
-# nt_24 = pd.read_csv("NAFTRAC_20221230.csv",skiprows=2).iloc[:-1 , :]
-# nt_25 = pd.read_csv("NAFTRAC_20230125.csv",skiprows=2).iloc[:-1 , :]
+import functions as fn
 
-from os import listdir, path
-from os.path import isfile, join
-import numpy as np
-import pandas_datareader.data as web
-from scipy.optimize import minimize
+all_files = fn.import_files('files/*.csv')
 
 
 
--- main.py
+++ main.py
@@ -5,35 +5,9 @@ Created on Mon Jan 30 19:51:46 2023
 @author: Tania
 """
 
-from data import nt_1, nt_2, nt_3, nt_4, nt_5, nt_6, nt_7, \
-    nt_8, nt_9, nt_10, nt_11, nt_12, nt_13, nt_14, nt_15, \
-    nt_16, nt_17, nt_18, nt_19, nt_20, nt_21, nt_22, nt_23, \
-    nt_24, nt_25
+import functions as fn
+from data import all_files
 
-common_tickers = set(nt_1['Ticker'].values).intersection(set(nt_2['Ticker'].values))\
-                    .intersection(set(nt_3['Ticker'].values))\
-                    .intersection(set(nt_4['Ticker'].values))\
-                    .intersection(set(nt_5['Ticker'].values))\
-                    .intersection(set(nt_6['Ticker'].values))\
-                    .intersection(set(nt_7['Ticker'].values))\
-                    .intersection(set(nt_8['Ticker'].values))\
-                    .intersection(set(nt_9['Ticker'].values))\
-                    .intersection(set(nt_10['Ticker'].values))\
-                    .intersection(set(nt_11['Ticker'].values))\
-                    .intersection(set(nt_12['Ticker'].values))\
-                    .intersection(set(nt_13['Ticker'].values))\
-                    .intersection(set(nt_14['Ticker'].values))\
-                    .intersection(set(nt_15['Ticker'].values))\
-                    .intersection(set(nt_16['Ticker'].values))\
-                    .intersection(set(nt_17['Ticker'].values))\
-                    .intersection(set(nt_18['Ticker'].values))\
-                    .intersection(set(nt_19['Ticker'].values))\
-                    .intersection(set(nt_20['Ticker'].values))\
-                    .intersection(set(nt_21['Ticker'].values))\
-                    .intersection(set(nt_22['Ticker'].values))\
-                    .intersection(set(nt_23['Ticker'].values))\
-                    .intersection(set(nt_24['Ticker'].values))\
-                    .intersection(set(nt_25['Ticker'].values))
-                    
-print(common_tickers)
-print(len(common_tickers))
+
+print(fn.find_tickers(all_files,'Ticker'))
+print(len(fn.find_tickers(all_files,'Ticker')))
