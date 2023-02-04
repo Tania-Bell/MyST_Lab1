4a0ff3b1221fc8db3828fc633006e39c50a49afe
Tania-Bell <taniabell1@hotmail.com>
2023-02-03 18:37:51 -0600

    Import prices, titles function
    Prices were imported with yfinance,
    the function of titles of the passive investment was created

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ Created on Mon Jan 30 19:51:46 2023
 import functions as fn
 from data import all_files
 
-common_tickers, all_files = fn.find_tickers(all_files,'Ticker')
-print(common_tickers)
-print(all_files)
+common_tickers, all_files = fn.find_tickers(all_files,'Ticker','Peso (%)')
+
+prices=fn.import_prices(all_files,common_tickers,'2021-01-29','2023-01-26')
+
+cash_w=float(all_files['20210129'][all_files['20210129']['Ticker'].str.contains("MXN")]['Peso (%)'])
+cap_titles = fn.titles(all_files,prices,cash_w)
+
+print(cap_titles)
