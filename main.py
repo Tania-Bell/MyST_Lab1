1e009ba203198bf6cf938a412b4c0f75cb1d6397
Tania-Bell <taniabell1@hotmail.com>
2023-02-08 12:53:16 -0600

    Comission and rebalance functions
    Comission function was merged with rebalance functions in order to get the correct iterations and conditions.

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ comission_rate = 0.00125
 
 rf=.1106
 
+r_change = 0.05
+
+r_rebalance = 0.025
+
 all_files = fn.import_files('files/*.csv')
 
 common_tickers, all_files = fn.find_tickers(all_files,'Ticker','Peso (%)')
@@ -25,18 +29,16 @@ cap_shares_p = fn.shares_passive(all_files,prices,cash_w,'20210129').round()
 
 comission_p = fn.comission_p(comission_rate,prices,cap_shares_p,'20210129')
 
-returns_ticker_p, df_pasiva = fn.rend_p(prices,cap_shares_p,cash_w,comission_p)
+returns_ticker_p, df_pasiva = fn.rend_p(prices,cap_shares_p,cash_w)
 
-weights = fn.sharpe(prices,rf)
+prices_daily = fn.import_prices_d(common_tickers,'2021-01-31','2022-01-31')
+
+weights = fn.sharpe(prices_daily,rf)
 
 cap_shares_a = fn.shares_active(weights, prices, cash_w, '20210129').round()
 
-shares_rebal_a = fn.rebalance(prices,cap_shares_a,0.05,0.025)
+capital_hist, shares_hist, comission_hist, shares_diff_hist = fn.rebalanceo(cash_w,cap_shares_a,prices,r_change,r_rebalance,comission_rate)
 
-df_activa = fn.rend_a(cap_shares_a,prices,cash_w)
+df_activa = fn.rend_a(capital_hist,cash_w)
 
-comission_a = fn.comission_a(comission_rate,prices,shares_rebal_a,cash_w)
-
-df_operaciones = fn.operations(shares_rebal_a,comission_rate,prices,comission_a)
-
-print(comission_a)
+df_operaciones = fn.operations(shares_hist,comission_hist,shares_diff_hist)
